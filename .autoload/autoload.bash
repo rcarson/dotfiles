7e674c41fb3a4849357ef4491a00740a3e35a956
Robert Carson <robert.carson@gmail.com>
2016-12-12 19:45:41 -0800

    updated for shellcheck issues

--- .autoload/autoload.bash
+++ .autoload/autoload.bash
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
+
 function update_remote_autoload_files ()
 {
-    cd ${AUTOLOAD_DIR:-${HOME}/.autoload}
+    cd "${AUTOLOAD_DIR:-${HOME}/.autoload}" || return
 
     # loop through list of files
     for remote_url in "${REMOTE_AUTOLOAD_FILES[@]}"; do
-	destination=${remote_url#*|}   # grab second value
-	remote_url=${remote_url%|*}    # grab first value
+	destination="${remote_url#*|}"   # grab second value
+	remote_url="${remote_url%|*}"    # grab first value
 
 
 	if [[ "$remote_url" == "$destination" ]]; then
@@ -14,12 +16,12 @@ function update_remote_autoload_files ()
 	fi    
 
 	con_message "Updating ${destination}"
-	if curl -fsSL ${destination:+-o $destination} $remote_url; then
+	if curl -fsSL ${destination:+-o $destination} "$remote_url" 2>/dev/null; then
 	    con_success
 	else
 	    con_failure
 	fi
     done
 
-    cd ${OLDPWD}
+    cd "${OLDPWD}" || return
 }
--- .autoload/console_messages.bash
+++ .autoload/console_messages.bash
@@ -1,24 +1,28 @@
+#!/usr/bin/env bash
+
 declare -i __CON_DEFAULT_STATUS_COLUMNS__=10
 declare -i __CON_DEFAULT_MAX_COLUMNS__=79
 
 function __con_max_columns ()
 {
-    [[ $COLUMNS -ge $__CON_DEFAULT_MAX_COLUMNS__ ]] && {
-	echo $__CON_DEFAULT_MAX_COLUMNS__
+    (( COLUMNS >= __CON_DEFAULT_MAX_COLUMNS__ )) && {
+	echo "$__CON_DEFAULT_MAX_COLUMNS__"
 	return
     }
-    echo $COLUMNS
+    echo "$COLUMNS"
 }
 
 function __con_status_columns ()
 {
-    echo ${CON_STATUS_COLUMNS:-$__CON_DEFAULT_STATUS_COLUMNS__}
+    echo "${CON_STATUS_COLUMNS:-$__CON_DEFAULT_STATUS_COLUMNS__}"
 }
 
 function __con_max_message_width ()
 {
-    local -i max_width=$(__con_max_columns)
-    local -i status_width=$(__con_status_columns)
+    local -i max_width status_width
+    max_width=$(__con_max_columns)
+    status_width=$(__con_status_columns)
+
     echo $((max_width-status_width))
 }
 
@@ -42,8 +46,9 @@ function __con_inflight ()
 function __con_status_message ()
 {
     local message="${*}"
-    local status_cols=$(__con_status_columns)
-    local max_columns=$(__con_max_columns)
+    local -i max_columns
+    max_columns=$(__con_max_columns)
+
     echo "$(tput cuf $((max_columns-__CON_CUR_COLUMN__)))$(tput cub $((${#message}+1)))$message" && {
 	__con_unset_cur_column
     }
@@ -55,7 +60,8 @@ function con_message ()
     __con_inflight && echo
 
     local message="${*}"
-    local message=${message:0:$(__con_max_message_width)}
+
+    message=${message:0:$(__con_max_message_width)}
     echo -n "$message" && {
 	__con_set_cur_column "${#message}"
     } || echo
